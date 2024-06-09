996b52ebf206abc01862f30829c3076d9c1674e5
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro.local>
2024-06-09 18:19:39 +0900

    20240609_作業

---
+++ pages/blog/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- {{ data }} -->
+    <p>This is Enginere Life</p>
+    <div v-for="singleData in data" :key="singleData.id">
+      <div>
+        <h3>{{ singleData.title }}</h3>
+        <p>{{ singleData.excerpt }}</p>
+        <p>{{ singleData.date }}</p>
+        <NuxtLink :to="singleData._path">Read More</NuxtLink>
+      </div>
+      <div>
+        <nuxt-img :src="singleData.image" alt="blog-image" format="webp" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("blogQuery", () =>
+  queryContent("/blog").find({ id: -1 })
+);
+console.log("data");
+console.log(data.value);
+</script>
+<style>
+h1 {
+  color: red;
+}
+.text {
+  color: green;
+  letter-spacing: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>IndexPage</h1>
+</template>
